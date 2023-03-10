732446d70ee52be903328388cc23cdfd5ccd22f4
Darko Antoski <antoskidarko24@gmail.com>
2023-03-10 17:03:49 +0100

    sefte

---
+++ quiz/Project_Demo_Quiz_App/index.js
@@ -0,0 +1,29 @@
+const form = document.querySelector(".quiz-form");
+const correctAnswers = ["A", "D", "B", "A", "C"];
+const result = document.querySelector(".result");
+const questions = document.querySelectorAll(".question")
+
+function reloadButton(){
+    window.location.reload();
+} 
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    
+    let score = 0;
+    const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value, form.q5.value]
+    
+    userAnswers.forEach((answer, index) => {
+      if(answer === correctAnswers[index]){
+        score += 1;
+        questions[index].classList.add("correct");
+      } else {
+        questions[index].classList.add("wrong");
+      }
+    
+    scrollTo(0,0);
+    result.classList.remove("hide");
+    result.querySelector("p").textContent = `You scored ${score}/5!`
+
+    })
+});
